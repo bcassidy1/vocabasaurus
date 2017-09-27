ec8254c908e49607117d9e947adb4a98a27eac11
Bancroft <bcassidy@aapl000615.local>
2017-09-27 08:25:40 -0400

    finished programming the buttons to say hi and bye

--- Vocabasaurus/ViewController.swift
+++ Vocabasaurus/ViewController.swift
@@ -12,14 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        label.text = "hi!!!!!!!!!!!"
+   
     }
 
 
+    @IBAction func startbutton(_ sender: Any) {
+        
+       label.text = "hi!!!!!!!!!!"
+    }
+    
+    
+    @IBAction func stop(_ sender: Any) {
+    
+    label.text = "bye!!!!!!!!!" 
+    
+    }
+    
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
 }
 
