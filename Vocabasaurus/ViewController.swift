1e3aaa9e95d40d389f2ff1cc0b7ee2a2596a536c
Bancroft <bcassidy@aapl000615.local>
2017-10-10 08:53:30 -0400

    we got the arrays of questions and answers

--- Vocabasaurus/ViewController.swift
+++ Vocabasaurus/ViewController.swift
@@ -9,29 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    var questions = ["feeling or showing pleasure or contentment", "feeling or showing sorrow; unhappy", "a usually sweet food (such as a blueberry, orange, or apple) that grows on a tree or bush", "a fossil reptile of the Mesozoic era, often reaching an enormous size"]
+    
+    var answers = [["mad", "happy", "sad"], ["mad", "happy", "sad"], ["vegetables", "fruit", "computers"],["dinosaurs", "llamas", "tigers"]]
+    
+    
+    //variables for every piece of text that's going to change - labels and buttons
+    
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var startbutton: UIButton!
+    @IBOutlet weak var stop: UIButton!
+    @IBOutlet weak var button1: UIButton!
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        label.text = "hi!!!!!!!!!!!"
-   
-    }
 
+// all functions that are triggered when the button is pressed
 
     @IBAction func startbutton(_ sender: Any) {
         
-       label.text = "hi!!!!!!!!!!"
     }
     
     
     @IBAction func stop(_ sender: Any) {
     
-    label.text = "bye!!!!!!!!!" 
+    }
+    
+    @IBAction func button1(_ sender: Any) {
     
     }
     
     
-    
-    @IBOutlet weak var label: UILabel!
-    
+
+override func viewDidLoad() {
+    super.viewDidLoad()
+    label.text = questions[0]
+    startbutton.setTitle(answers[0][0], for: .normal)
+
+
+
+
+
+
+
+
+}
 }
 
