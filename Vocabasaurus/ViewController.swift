942b0ce13fb3d76b1fc925f545d8aa2b53fa41db
Bancroft <bcassidy@aapl000615.local>
2017-10-13 09:16:09 -0400

    we got the questions to print and the buttons to work

--- Vocabasaurus/ViewController.swift
+++ Vocabasaurus/ViewController.swift
@@ -11,14 +11,16 @@ import UIKit
 class ViewController: UIViewController {
     
     //variable that says what the current question is
-    var questionNumber = 0
+   
     
     var questions = ["feeling or showing pleasure or contentment", "feeling or showing sorrow; unhappy", "a usually sweet food (such as a blueberry, orange, or apple) that grows on a tree or bush", "a fossil reptile of the Mesozoic era, often reaching an enormous size"]
     
     var answers = [["mad", "happy", "sad"], ["mad", "happy", "sad"], ["vegetables", "fruit", "computers"],["dinosaurs", "llamas", "tigers"]]
     
-    //TODO #2 - Create an array of the index of all the correct answers
-    
+
+    var correctAnswers = [1, 2, 1, 0]
+    var questionNumber = 0
+
     
     //variables for every piece of text that's going to change - labels and buttons
     @IBOutlet weak var label: UILabel!
@@ -31,12 +33,15 @@ class ViewController: UIViewController {
     /* TODO #4 - If the questionNumber is less than than the length of the array, increase the question number by one. If it's not reset it back to 0. Then show and hide the correct buttons. */
     @IBAction func nextQuestion(_ sender: Any) {
         if (questionNumber < questions.count - 1){
-            //TODO
+            questionNumber += 1
         }
         else {
-            //TODO
+            questionNumber = 0
         }
-        //TODO
+        button1.isHidden = false
+        button2.isHidden = false
+        button3.isHidden = false
+        nextQuestion.isHidden = true
     }
     
 // all functions that are triggered when the button is pressed
@@ -49,7 +54,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func button3(_ sender: Any) {
-        //TODO
+        checkAnswer(buttonNumber: 2)
     }
     
     
@@ -70,15 +75,31 @@ override func viewDidLoad() {
     func displayQuestions() {
         label.text = questions[questionNumber]
         button1.setTitle(answers[questionNumber][0], for: .normal)
-        //TODO #1 - finish the setTitles for last two buttons
+        button2.setTitle(answers[questionNumber][1], for: .normal)
+        button3.setTitle(answers[questionNumber][2], for: .normal)
+        
+
     }
     
-    //TODO #3 - Finish the checkAnswer function. First finish the function call for button3. Then create an if/then/else statement. If the answer is correct print something good. If not, print something bad. Then hide the 3 answer buttons and show the nextQuestion button.
-    
+	
     func checkAnswer(buttonNumber: Int){
-        print(buttonNumber)
-        //TODO
+        button1.isHidden = true
+        button2.isHidden = true
+        button3.isHidden = true
+        nextQuestion.isHidden = false
+        
+        if (buttonNumber == correctAnswers[questionNumber]){
+            label.text = "good job!!!!!!!!"
+            
+        }
+            
+            
+        else{
+            label.text = "You are terrible at everything"
+        }
+        
     }
     
+
 }
 
